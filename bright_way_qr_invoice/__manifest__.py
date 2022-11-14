7dc41383cf190d27dfd0f989845cfa5a2878c2b7
toqa <toqa@centione.com>
2022-11-14 11:32:18 +0200

    invoices report edit

--- bright_way_qr_invoice/__manifest__.py
+++ bright_way_qr_invoice/__manifest__.py
@@ -27,6 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/invoices_report_edit.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
