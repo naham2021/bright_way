1384d5211994f5536d115a23eacb36195dd10e33
toqaelgazzar <toqahosam59@gmail.com>
2024-01-16 14:05:38 +0200

    New report

---
+++ brightway_quotation_offer_report/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "brightway_quotation_offer_report",
+    'author': "Centione",
+    'website': "",
+    'depends': ['base', 'sale'],
+    'data': [
+        'report/sale_quotation_offer_report.xml',
+    ],
+
+}
